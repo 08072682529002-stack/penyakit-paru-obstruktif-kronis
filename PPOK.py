526774bae8ed467f1ef9662edf8842184aa0cb47
Rahma Yuliana <08072682529002@student.unsri.ac.id>
2025-11-12 14:47:12 +0700

    Update PPOK.py

--- PPOK.py
+++ PPOK.py
@@ -305,7 +305,15 @@ elif menu == "🩺 Diagnosis PPOK":
             st.metric("Ketidaktahuan (Theta)", f"{belief_theta*100:.2f}%")
             st.progress(belief_theta)
 
-            st.info(f" Nama: {nama} | Umur: {umur} tahun n/ Gejala: " + ", ".join([symptom_names[c] for c in selected]) n/ Waktu: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            st.info(
+    f"""
+    👤 **Nama:** {nama}  
+    🎂 **Umur:** {umur} tahun  
+    🩺 **Gejala:** {", ".join([symptom_names[c] for c in selected])}  
+    🕒 **Waktu:** {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+    """
+)
+
 
             if belief_ppok >= 0.625:
                 st.success("🟢 Indikasi PPOK tinggi — segera konsultasikan ke dokter spesialis paru.")
